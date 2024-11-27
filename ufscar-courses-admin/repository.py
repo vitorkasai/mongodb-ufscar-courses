452e72c8b569a5a0c5a02fe8011acee65c9ea26e
Vitor Kasai <vitorkasai@estudante.ufscar.br>
2024-11-27 19:51:21 -0300

    feat: update e delete a partir da listagem

--- ufscar-courses-admin/repository.py
+++ ufscar-courses-admin/repository.py
@@ -33,22 +33,44 @@ def find_cursos_by_tipo(tipo_curso):
 
 def find_cursos_by_nome(nome_curso):
     collection = get_db_connection()
-    cursos_encontrados = collection.find(
-        {"nome_curso": {"$regex": nome_curso, "$options": "i"}}, {"_id": 0}
-    )
+    cursos_encontrados = collection.find({"nome_curso": {"$regex": nome_curso, "$options": "i"}}, {"_id": 0})
     return cursos_encontrados
 
 
 def find_all_campus():
     collection = get_db_connection()
-    return collection.distinct("campus_curso")
+    campus_list = collection.distinct("campus_curso")
+    return campus_list
 
 
 def find_all_tipo_curso():
     collection = get_db_connection()
-    return collection.distinct("tipo_curso")
+    tipo_list = collection.distinct("tipo_curso")
+    return tipo_list
+
+
+def atualizar_curso_no_banco(curso, novos_dados):
+    collection = get_db_connection()
+    resultado = collection.update_one(
+        {"nome_curso": curso['nome_curso'], "campus_curso": curso['campus_curso']},
+        {"$set": novos_dados}
+    )
+    if resultado.matched_count:
+        print("Curso atualizado com sucesso.")
+    else:
+        print("Curso não encontrado.")
+
+
+def remover_curso_do_banco(curso):
+    collection = get_db_connection()
+    resultado = collection.delete_one({"nome_curso": curso['nome_curso'], "campus_curso": curso['campus_curso']})
+    if resultado.deleted_count:
+        print("Curso removido com sucesso.")
+    else:
+        print("Curso não encontrado.")
 
 
 def inserir_novo_curso(curso):
     collection = get_db_connection()
-    collection.insert_one(curso)
+    resultado = collection.insert_one(curso)
+    return resultado
