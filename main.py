dd26396cde4aa8ee4217dbebc88a5a50d427ec25
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2024-12-04 01:31:12 -0300

    feat: habilitando exportação de dados via JSON/CSV

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from utils import encerrar_programa, exibir_menu, admin_menu_opcoes, executar_crud, cores
+from utils import encerrar_programa, exibir_menu, admin_menu_opcoes, executar_crud, cores, tables, export_data
 from helpers import eh_indice_valido
 
 def main():
@@ -19,7 +19,25 @@ def main():
             print(f"{cores['verde']}\nMenu acessado: {admin_menu_opcoes[tabela_escolhida]['opcao']}{cores['reset']}")
             executar_crud(tabela_escolhida)
         else:
-            print(f"{cores['vermelho']}Opção inválida. Por favor, escolha uma opção válida.{cores['reset']}")
+            if escolha.isdigit() and int(escolha) in [4, 5]:
+                formato = "csv" if int(escolha) == 4 else "json"
+                print(f"Tabelas disponíveis para exportar em {formato.upper()}:")
+                for indice, nome_tabela in enumerate(tables.values(), 1):
+                    print(f"{indice}. {nome_tabela.capitalize()}")
+                escolha_tabela = input("Escolha uma opção (deixe vazio para voltar): ").strip().lower()
+                while not (eh_indice_valido(escolha_tabela, len(tables)) or escolha_tabela == ""):
+                    print(f"{cores['vermelho']}Opção inválida. Por favor, escolha uma opção válida.{cores['reset']}")
+                    escolha_tabela = input("Escolha uma opção (deixe vazio para voltar): ").strip().lower()
+                if escolha_tabela != "":
+                    indice = int(escolha_tabela) - 1
+                    tabela_escolhida = list(tables.values())[indice]
+                    print(f"{cores['verde']}\nExportando dados da tabela {tabela_escolhida.capitalize()} para {formato.upper()}...{cores['reset']}")
+                    if formato == 'csv':
+                        export_data(tabela_escolhida, 'csv')
+                    else:
+                        export_data(tabela_escolhida, 'json')
+            else:
+                print(f"{cores['vermelho']}Opção inválida. Por favor, escolha uma opção válida.{cores['reset']}")
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@
 
 from services import *
 from constants import admin_menu_opcoes, tables, cores
+from repositories import get_db_connection
+import json
+import os
+import csv
 
 from services.curso import exibir_menu_curso
 from services.semestre import exibir_menu_semestre
@@ -18,7 +22,11 @@ def exibir_menu() -> None:
     for key, value in admin_menu_opcoes.items():
         print(f"{indice}. {value['opcao']}")
         indice += 1
+    # Adicionando opções a mais para download csv/json
+    print(f"{indice}. Exportar dados para CSV")
+    print(f"{indice + 1}. Exportar dados para JSON")
     print(f"{cores['vermelho']}0. Encerrar programa{cores['reset']}")
+
 
 
 def executar_crud(table):
@@ -31,3 +39,73 @@ def executar_crud(table):
         exibir_menu_professor()
     else:
         print(f"{cores['vermelho']}Tabela não reconhecida.{cores['reset']}")
+
+
+def ensure_directory_exists(path):
+    """Cria o diretório de destino, se ele não existir."""
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def export_to_csv(collection, file_name):
+    """Exporta os dados da coleção para um arquivo CSV, considerando todos os campos."""
+    cursor = collection.find()
+    all_fieldnames = set()  # Usar um set para garantir que as chaves sejam únicas
+
+    # Itera sobre todos os documentos e coleta todas as chaves
+    for document in cursor:
+        all_fieldnames.update(document.keys())
+
+    # Voltar ao início da consulta para reescrever os dados no CSV
+    cursor = collection.find()
+
+     # Definindo o caminho do diretório
+    export_dir = './exported_data/csv'
+    ensure_directory_exists(export_dir)
+    
+    # Definindo o caminho completo para o arquivo CSV
+    file_path = os.path.join(export_dir, f"{file_name}")
+
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=all_fieldnames)
+        writer.writeheader()
+        for document in cursor:
+            writer.writerow(document)
+
+def export_to_json(collection, file_name):
+    """Exporta os dados da coleção para um arquivo JSON, lidando com ObjectId."""
+    cursor = collection.find()
+    documents = list(cursor)
+
+    # Função para converter ObjectId para string
+    def convert_objectid(document):
+        for key, value in document.items():
+            if isinstance(value, ObjectId):
+                document[key] = str(value)  # Converte ObjectId para string
+        return document
+
+    # Converte cada documento para garantir que ObjectId seja serializado corretamente
+    documents = [convert_objectid(doc) for doc in documents]
+
+     # Definindo o caminho do diretório
+    export_dir = './exported_data/json'
+    ensure_directory_exists(export_dir)
+    
+    # Definindo o caminho completo para o arquivo JSON
+    file_path = os.path.join(export_dir, f"{file_name}")
+
+    # Salvando os documentos no arquivo JSON
+    with open(file_path, 'w', encoding='utf-8') as jsonfile:
+        json.dump(documents, jsonfile, indent=4, ensure_ascii=False)
+
+def export_data(collection_name, export_format):
+    """Exporta os dados da coleção escolhida para o formato desejado."""
+    collection = get_db_connection(collection_name)
+
+    if export_format == 'csv':
+        file_name = f"{collection_name}.csv"
+        export_to_csv(collection, file_name)
+        print(f"{cores['verde']}Dados exportados para {file_name}{cores['reset']}")
+    elif export_format == 'json':
+        file_name = f"{collection_name}.json"
+        export_to_json(collection, file_name)
+        print(f"{cores['verde']}Dados exportados para {file_name}{cores['reset']}")
